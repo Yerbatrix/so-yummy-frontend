b30675019ac28e55ad0a8c6f514d7a211041affb
klaudiab <klaudiabalas1303@gmail.com>
2024-08-04 16:20:51 +0200

    Update operations.js

--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -1,13 +1,14 @@
+// operations.js
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://t4-soyummy-api-2752d40c2586.herokuapp.com/";
 
 export const getCategoryList = createAsyncThunk(
-  "categories/fetchCategoryList",
+  "categories/getCategoryList",
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get(`/recipes/category-list`);
+      const { data } = await axios.get("/recipes/category-list");
       return data.categories;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
