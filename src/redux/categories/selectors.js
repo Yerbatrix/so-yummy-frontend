05f1e54107b2c061ad0bf394f983362585b51ab1
Karolfm12 <karolfm12@onet.eu>
2024-08-04 20:23:58 +0200

    Categories#1

--- src/redux/categories/selectors.js
+++ src/redux/categories/selectors.js
@@ -1,3 +1,4 @@
 export const selectCategoryList = (state) => state.categories.categoryList;
 export const selectIsLoading = (state) => state.categories.isLoading;
 export const selectError = (state) => state.categories.error;
+// export const selectRecipes = (state) => state.recipes.recipes;
