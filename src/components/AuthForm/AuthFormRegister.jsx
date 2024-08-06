77c00448a4dd27d97426619754de0fda7f1cd4ac
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-08-06 21:37:28 +0200

    background+inputs

--- src/components/AuthForm/AuthFormRegister.jsx
+++ src/components/AuthForm/AuthFormRegister.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+
+import axios from "axios";
 import { FaUser } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
   Button,
@@ -15,7 +16,6 @@ import {
   SignLink,
   Title,
 } from "./AuthForm.styled";
-import axios from "axios";
 
 const AuthFormRegister = () => {
   const [formState, setFormState] = useState({
@@ -23,6 +23,12 @@ const AuthFormRegister = () => {
     email: "",
     password: "",
   });
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [emailBorderColor, setEmailBorderColor] = useState("#F6C23E");
+  const [nameBorderColor, setNameBorderColor] = useState("#F6C23E");
+  const [passwordBorderColor, setPasswordBorderColor] = useState("#F6C23E");
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
@@ -52,7 +58,43 @@ const AuthFormRegister = () => {
       setErrors({ message: error.response.data.msg });
     }
   };
+  const verifyName = (event) => {
+    const { value } = event.currentTarget;
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    if (value.length < 3 || !nameRegex.test(value)) {
+      setIsNameValid(false);
+      setNameBorderColor("red");
+    } else {
+      setIsNameValid(true);
+      setNameBorderColor("green");
+    }
+  };
+  const verifyEmail = (event) => {
+    const { value } = event.currentTarget;
+    if (value.length === 0 || !value.includes("@")) {
+      setIsEmailValid(false);
+      setEmailBorderColor("red");
+    } else {
+      setIsEmailValid(true);
+      setEmailBorderColor("green");
+    }
+  };
 
+  const verifyPassword = (event) => {
+    const { value } = event.currentTarget;
+    if (
+      value.length === 0 ||
+      !/[A-Z]/.test(value) ||
+      !/[a-z]/.test(value) ||
+      !/\d/.test(value)
+    ) {
+      setIsPasswordValid(false);
+      setPasswordBorderColor("red");
+    } else {
+      setIsPasswordValid(true);
+      setPasswordBorderColor("green");
+    }
+  };
   return (
     <Container>
       <Form autoComplete="off" onSubmit={handleSubmit}>
@@ -66,9 +108,14 @@ const AuthFormRegister = () => {
                 placeholder="Name"
                 value={formState.name}
                 onChange={handleChange}
+                onBlur={verifyName}
                 required
+                style={{
+                  borderColor: nameBorderColor,
+                  borderWidth: "2px",
+                }}
               />
-              <Icon as={FaUser} color="fff" mr={5} />
+              <Icon as={FaUser} color={nameBorderColor} mr={5} />
             </List>
             <List>
               <Input
@@ -77,9 +124,14 @@ const AuthFormRegister = () => {
                 placeholder="Email"
                 value={formState.email}
                 onChange={handleChange}
+                onBlur={verifyEmail}
                 required
+                style={{
+                  borderColor: emailBorderColor,
+                  borderWidth: "2px",
+                }}
               />
-              <Icon as={MdOutlineEmail} color="fff" mr={5} />
+              <Icon as={MdOutlineEmail} color={emailBorderColor} mr={5} />
             </List>
             <List>
               <Input
@@ -88,15 +140,22 @@ const AuthFormRegister = () => {
                 name="password"
                 value={formState.password}
                 onChange={handleChange}
+                onBlur={verifyPassword}
                 required
+                style={{
+                  borderColor: passwordBorderColor,
+                  borderWidth: "2px",
+                }}
               />
-              <Icon as={RiLockPasswordLine} color="fff" mr={5} />
+              <Icon
+                as={RiLockPasswordLine}
+                color={passwordBorderColor}
+                mr={5}
+              />
             </List>
           </ContainerForm>
         </div>
-
-        {errors.message && <p>{errors.message}</p>}
-
+        {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
         <Button type="submit">Sign up</Button>
       </Form>
       <SignLink to="/signin"> Sign in </SignLink>
