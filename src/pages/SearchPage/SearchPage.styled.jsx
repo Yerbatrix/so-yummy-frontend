b60842b8ea2166f0760f3861f510e65022689172
michallow <michallo_w@o2.pl>
2024-08-07 15:22:02 +0200

    Search_Branch - poprawki
    Naprawione wyszukiwanie (teraz już na start szuka po "title")
    
    Dodane stylowanie

--- src/pages/SearchPage/SearchPage.styled.jsx
+++ src/pages/SearchPage/SearchPage.styled.jsx
@@ -20,19 +20,91 @@ export const SearchPageTitle = styled.h2`
 `;
 
 export const SearchPageContainer = styled.div`
-width: 100%;
-margin: 0 auto;
-padding: 0 16px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
 
-@media (min-width: 375px) {
-  width: 375px;
-}
-@media (min-width: 768px) {
-  padding: 0 32px;
-  width: 768px;
-}
-@media (min-width: 1440px) {
-  padding: 0 100px;
-  width: 1440px;
-}
+  @media (min-width: 375px) {
+    width: 375px;
+  }
+  @media (min-width: 768px) {
+    padding: 0 32px;
+    width: 768px;
+  }
+  @media (min-width: 1440px) {
+    padding: 0 100px;
+    width: 1440px;
+  }
+`;
+
+export const ResultsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+`;
+
+export const ResultItem = styled.div`
+  width: calc(25% - 16px); // Domyślnie 4 w rzędzie
+  box-sizing: border-box;
+  position: relative;
+  
+  @media (max-width: 1024px) {
+    width: calc(50% - 16px); // 2 w rzędzie dla tabletów
+  }
+  
+  @media (max-width: 768px) {
+    width: 100%; // 1 w kolumnie dla mobile
+  }
+  
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+  
+  .title-container {
+    position: absolute;
+    bottom: 6%;
+    left: 5%;
+    right: 5%;
+    height: 60px;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    box-sizing: border-box;
+    text-align: left; // Dodanie wyrównania tekstu do lewej
+    
+    p {
+      margin: 0;
+      font-size: 16px;
+      font-weight: 500;
+      color: #000;
+    }
+  }
+`;
+
+export const NoResults = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+
+  img {
+    width: 30%;
+    height: auto;
+    border-radius: 8px;
+  }
+
+  p {
+    margin-top: 10px;
+    font-size: 16px;
+    color: #666;
+  }
 `;
