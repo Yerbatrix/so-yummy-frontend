0c2c0d4ecf27c987c62b8b9bd2cc7dbbc7b6ec4e
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-08-08 13:37:42 +0200

    add favorites endpoints + loader + sadface :)

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,229 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "../../redux/axiosConfig";
 import ReactPaginate from "react-paginate";
+import Loader from "../../components/Loader/Loader";
 import "./Favorites.css";
 import deleteIconMobile from "../../images/deleteBttn/delete-bttn-mobile.png";
 import deleteIconTablet from "../../images/deleteBttn/delete-bttn-tablet.png";
 import deleteIconDesktop from "../../images/deleteBttn/delete-bttn-desktop.png";
-
+import sadFace from "../../images/sadFace/sad-face.jpg";
 
 const Favorites = () => {
-  const recipes = [
-    {
-      id: 1,
-      title: "Spaghetti Carbonara",
-      description:
-        "A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper. A classic Italian pasta dish with eggs, cheese, pancetta, and pepper",
-      image:
-        "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
-      preparationTime: "30 min",
-    },
-    {
-      id: 2,
-      title: "Chicken Alfredo",
-      description:
-        "Creamy Alfredo sauce with tender chicken pieces and fettuccine pasta.",
-      image:
-        "https://www.themealdb.com/images/media/meals/vtxyxv1483567157.jpg",
-      preparationTime: "45 min",
-    },
-    {
-      id: 3,
-      title: "Caesar Salad",
-      description:
-        "Crisp romaine lettuce, Caesar dressing, croutons, and Parmesan cheese.",
-      image:
-        "https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg",
-      preparationTime: "20 min",
-    },
-    {
-      id: 4,
-      title: "Margherita Pizza",
-      description:
-        "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
-      image:
-        "https://www.themealdb.com/images/media/meals/rvypwy1503069308.jpg",
-      preparationTime: "1 hr",
-    },
-    {
-      id: 5,
-      title: "Taco Salad",
-      description:
-        "A delicious taco salad with ground beef, lettuce, cheese, and salsa.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "25 min",
-    },
-    {
-      id: 6,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 7,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 8,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 9,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 10,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 11,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 12,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 13,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 14,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 15,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 16,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 17,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 18,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 19,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 20,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 31,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 22,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 23,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 24,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 25,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 26,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 27,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 28,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-    {
-      id: 29,
-      title: "Lasagna",
-      description: "Layered pasta with meat, cheese, and tomato sauce.",
-      image: "https://via.placeholder.com/150",
-      preparationTime: "1 hr 15 min",
-    },
-  ];
-
+  const [recipes, setRecipes] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [loading, setLoading] = useState(true);
   const recipesPerPage = 4;
 
   useEffect(() => {
@@ -232,23 +21,62 @@ const Favorites = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/api/favorites");
+        setRecipes(response.data.data.results);
+      } catch (error) {
+        console.error("Error fetching the recipes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/api/favorites/${id}`);
+      setRecipes((prevRecipes) =>
+        prevRecipes.filter((recipe) => recipe._id !== id)
+      );
+    } catch (error) {
+      console.error("Error deleting the recipe:", error);
+    }
+  };
+
   const displayRecipes = recipes
     .slice(currentPage * recipesPerPage, (currentPage + 1) * recipesPerPage)
     .map((recipe) => (
-      <div key={recipe.id} className="recipe">
-        <img src={recipe.image} alt={recipe.title} className="recipe-image" />
+      <div key={recipe._id} className="recipe">
+        <img src={recipe.thumb} alt={recipe.title} className="recipe-image" />
         <div className="recipe-details">
           <h2>{recipe.title}</h2>
           <p className="recipe-description">{recipe.description}</p>
           <div className="timer-container">
-            <p className="prep-time">{recipe.preparationTime}</p>
+            <p className="prep-time">{recipe.time} min</p>
             <div className="recipe-actions">
               <button className="btn-see-fav">See recipe</button>
-              <button className="btn-delete-fav">
-              <picture>
-                  <source media="(min-width: 1024px)" srcSet={deleteIconDesktop} />
-                  <source media="(min-width: 768px)" srcSet={deleteIconTablet} />
-                  <img src={deleteIconMobile} alt="Delete" className="delete-icon" />
+              <button
+                className="btn-delete-fav"
+                onClick={() => handleDelete(recipe._id)}
+              >
+                <picture>
+                  <source
+                    media="(min-width: 1024px)"
+                    srcSet={deleteIconDesktop}
+                  />
+                  <source
+                    media="(min-width: 768px)"
+                    srcSet={deleteIconTablet}
+                  />
+                  <img
+                    src={deleteIconMobile}
+                    alt="Delete"
+                    className="delete-icon"
+                  />
                 </picture>
               </button>
             </div>
@@ -265,22 +93,37 @@ const Favorites = () => {
       <div className="header">
         <h1>Favorites</h1>
       </div>
-      <div className="recipes">{displayRecipes}</div>
-      <div>
-        <ReactPaginate
-          previousLabel={"<"}
-          nextLabel={">"}
-          pageCount={pageCount}
-          onPageChange={({ selected }) => setCurrentPage(selected)}
-          containerClassName={"pagination"}
-          previousLinkClassName={"pagination__link"}
-          nextLinkClassName={"pagination__link"}
-          disabledClassName={"pagination__link--disabled"}
-          activeClassName={"pagination__link--active"}
-          pageRangeDisplayed={pageRangeDisplayed}
-          marginPagesDisplayed={1}
-        />
-      </div>
+      {loading ? (
+        <div className="loading-container">
+          <Loader />
+        </div>
+      ) : recipes.length === 0 ? (
+        <div className="no-favorites">
+          <img src={sadFace} alt="Sad face" className="sad-face-image" />
+          <p>You have no favorite recipes added.</p>
+        </div>
+      ) : (
+        <>
+          <div className="recipes">{displayRecipes}</div>
+          {recipes.length > recipesPerPage && (
+            <div>
+              <ReactPaginate
+                previousLabel={"<"}
+                nextLabel={">"}
+                pageCount={pageCount}
+                onPageChange={({ selected }) => setCurrentPage(selected)}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+                pageRangeDisplayed={pageRangeDisplayed}
+                marginPagesDisplayed={1}
+              />
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
