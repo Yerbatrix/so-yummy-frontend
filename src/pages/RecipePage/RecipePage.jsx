269079c40e2282462a6f3847e700c72e722d60f4
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2024-08-09 12:52:04 +0200

    przeniesienie createAsyncThunk do RecipePage

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -6,7 +6,23 @@ import Loader from "../../components/Loader/Loader";
 
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
-import { getRecipesById } from "../../redux/RecipePage/operations";
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://t4-soyummy-api-2752d40c2586.herokuapp.com/";
+
+const getRecipesById = createAsyncThunk(
+  "recipes/getOneRecipe",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.get(`/recipes/${id}`, credentials);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 const RecipePage = () => {
   const [recipeObj, setRecipeObj] = useState(null);
