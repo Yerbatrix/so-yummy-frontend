678018087ad98c33cc9b288bb26634b8e103265d
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2024-08-12 12:27:32 +0200

    Update operations.js

--- src/redux/recipes/operations.js
+++ src/redux/recipes/operations.js
@@ -14,3 +14,39 @@ export const addOwnRecipe = createAsyncThunk(
     }
   }
 );
+
+export const addToFavorite = createAsyncThunk(
+  "recipes/addFavorite",
+  async (recipeId, thunkAPI) => {
+    try {
+      const response = await axios.post(`/api/favorite/${recipeId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteFavorite = createAsyncThunk(
+  "recipes/deleteFavorite",
+  async (recipeId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/api/favorite/${recipeId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const checkFavorite = createAsyncThunk(
+  "recipes/checkFavorite",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(`/api/favorite/`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
